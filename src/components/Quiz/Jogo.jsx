af81035966774ae4f5419fa93de7ca5faf485b3e
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-06 19:07:08 -0300

    Sem muitas mudanças

--- src/components/Quiz/Jogo.jsx
+++ src/components/Quiz/Jogo.jsx
@@ -1,46 +1,85 @@
 import React, { Component } from 'react';
+import "./jogo.css";
+import Questions from "./Q.json";
+
+import combo from "../../Assets/img/intersecao.png";
+import Lampada from "../../Assets/img/lampada-brilhante.png";
+import time from "../../Assets/img/relogio.png";
 
 export default class Jogo extends Component {
-  //constructor(props) {
-    //super(props);
-    //this.state = {
-      //Counter: 0
-    //};
-
-    // Bind the method to the class instance
-    //this.increseCount = this.increseCount.bind(this);
-  //}
-
-  increseCount() {
-    // Increment the counter instead of setting it to 5
-    this.setState((prevState) => ({
-      Counter: prevState.Counter + 1
-    }));
+  constructor(props) {
+    super(props);
+    this.state = {
+      question:[],
+      questionAtual:{},
+      nextQuestion:{},
+      questionAnterior:{},
+      resposta:"",
+      Numero_de_perguntas:0,
+      Numero_de_respondidas:0,
+      Numero_de_corretas:0,
+      Pergunta_atual:0,
+      pontação:0,
+      erradas:0,
+      Timer:{},
+    };
   }
+  
 
   render() {
+    console.log(Questions)
     return (
-      <div className="Pergunta">
-        <span className="Vidas" role="img" aria-label="Lifes">❤️️{}</span>
-        <h5>Em que ano o Google foi fundado?</h5>
-        <div className="Opções-Container">
+      
+      <>
+        <div className="Pergunta">
+          <div className="Heads">
+          <div className="Vidas">
+            <span  role="img" aria-label="Lifes">❤️️</span>
+            <p>3</p>
+          </div>
+          
+          <div className="Combo">
+            <img src={combo} alt="combo" />
+            <p>2</p>
+          </div>
 
-          <p className="opção"> 1999 </p>
-          <p className="opção"> 2000 </p>
+          <div className="Dica">
+            <img src={Lampada} alt="Ideia" />
+            <p>3</p>
+          </div>
+          
+          <div className="Numero_de_perguntas">
+            <p>
+              1 de X
+            </p>
+          </div>
+          <div className="Timer">
+            <p>
+              2:15
+            </p>
+            <img src={time} alt="Timer" />
+          </div>
+          </div>
+          <h2 className="Question">Em que ano o Google foi fundado?</h2>
+          <div className="Opções-Container">
 
+            <p className="opção"> 1999 </p>
+            <p className="opção"> 2000 </p>
+
+          </div>
+          <div className="Opções-Container">
+
+            <p className="opção"> 1998 </p>
+            <p className="opção"> 2001 </p>
+
+          </div>
+          <div className="container-botão">
+            <button> Anterior </button>
+            <button> Proxima </button>
+            <button> Quit </button>
+          </div>
         </div>
-        <div className="Opções-Container">
-
-          <p className="opção"> 1999 </p>
-          <p className="opção"> 2000 </p>
-
-        </div>
-        <div className="container-botão">
-          <button> Anterior </button>
-          <button> Proxima </button>
-          <button> Quit </button>
-        </div>
-      </div>
+      </>
     );
   }
 }
